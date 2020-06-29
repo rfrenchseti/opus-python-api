806629fe05a53dfea92ab1212da3fef4731f888e
Robert French <rfrench@seti.org>
2020-06-28 20:21:18 -0700

    Add get_metadata_raw, get_files_raw, get_images_raw

--- opusapi/opusapi.py
+++ opusapi/opusapi.py
@@ -3,6 +3,7 @@
 OPUSAPI class
 """
 
+from functools import wraps
 import json
 import pandas as pd
 import requests
@@ -11,9 +12,51 @@ import warnings
 from opusapi.util import CaseInsensitiveDict
 
 _DEFAULT_OPUS_SERVER = 'https://opus.pds-rings.seti.org'
+_DEFAULT_FIELDS = ['opusid']
+
+def hide_paging(data_name):
+    """Automatically retrieve pages from OPUS and yield them in one stream."""
+    # TODOAPI: The fact that we need to have a "data_name" here is an
+    # inconsistency in the API.
+    def _hide_paging(method):
+        @wraps(method)
+        def _impl(self, query=None, startobs=1, limit=None,
+                  paging_limit=100, **method_kwargs):
+            if startobs < 1:
+                raise ValueError
+            if limit is not None and limit < 1:
+                raise ValueError
+            count = 0
+            while limit is None or count < limit:
+                ret = method(self, query, startobs, paging_limit,
+                             **method_kwargs)
+                data = ret[data_name]
+                returned_count = ret['count']
+                # TODOAPI: The fact that we return both dicts and lists
+                # for different calls is an inconsistency in the API
+                if isinstance(data, dict):
+                    # For files.json and images.json
+                    for opusid, fields in data.items():
+                        yield {opusid: fields}
+                        count += 1
+                        if limit is not None and count >= limit:
+                            break
+                else:
+                    # For data.json
+                    for datum in data:
+                        yield datum
+                        count += 1
+                        if limit is not None and count >= limit:
+                            break
+                available = ret['available']
+                startobs += returned_count
+                if startobs > available:
+                    break
+        return _impl
+    return _hide_paging
 
 class OPUSAPI(object):
-    def __init__(self, server=None, verbose=False):
+    def __init__(self, server=None, default_fields=None, verbose=False):
         """Constructor for the OPUSAPI class."""
         self._verbose = verbose
 
@@ -26,6 +69,8 @@ class OPUSAPI(object):
                 server = 'https://' + server
         self._server = server
 
+        self._default_fields = (_DEFAULT_FIELDS if default_fields is None
+                                                else default_fields)
         self._raw_fields_cache = None
         self._raw_fields_as_df_cache = None
         self._fields_cache = None
@@ -354,3 +399,107 @@ class OPUSAPI(object):
         res = self._call_opus_api('meta/range/endpoints/'+fieldid, 'json',
                                   params=params)
         return res['min'], res['max'], res['nulls'], res['units']
+
+    ### Metadata, Files, Images API Calls
+
+    @property
+    def default_fields(self):
+        return self._default_fields
+
+    def _normalize_fields(self, fields):
+        if fields is None:
+            fields = self.default_fields
+        if isinstance(fields, str):
+            fields = fields.split(',')
+        raw_fields = self.raw_fields
+        for field in fields:
+            if field not in raw_fields:
+                raise RuntimeError(f'Unknown field id "{field}"')
+        return ','.join(fields)
+
+    def _normalize_product_types(self, product_types):
+        if product_types is None:
+            return None
+        # TODO: Read and store available product types like we do for fields
+        # and then validate them here
+        return ','.join(product_types)
+
+    @hide_paging('page')
+    def get_metadata_raw(self, query, startobs, limit, fields=None):
+        """Return the results of raw calls to data.json.
+
+        This returns a list. Each list element is a list of metadata
+        corresponding to the requested fields. All fields are returned
+        as strings regardless of the underlying field type.
+
+        Example:
+            [['co-iss-n1454939333', '2004-02-08T13:25:41.089', '18'],
+             ['co-iss-n1454939373', '2004-02-08T13:26:36.496', '2.6']]
+        """
+        params = {} if query is None else query.get_api_params(opusapi=self)
+        params['startobs'] = startobs
+        params['limit'] = limit
+        params['cols'] = self._normalize_fields(fields)
+        res = self._call_opus_api('data', 'json', params=params)
+        return res
+
+    @hide_paging('data')
+    def get_files_raw(self, query, startobs, limit, product_types=None):
+        """Return the results of raw calls to files.json.
+
+        This returns a list. Each list element is a dict where the key is
+        the OPUS ID and the value is a dict with keys as product types and
+        values as URLs.
+
+        Example:
+            [{'co-iss-n1454725799': {
+                [...]
+                'coiss_calib': [
+                    'https://pds-rings.seti.org/ ... N1454725799_1_CALIB.IMG',
+                    'https://pds-rings.seti.org/ ... N1454725799_1_CALIB.LBL'],
+                [...]
+             }]
+        """
+        params = {} if query is None else query.get_api_params(opusapi=self)
+        params['startobs'] = startobs
+        params['limit'] = limit
+        types = self._normalize_product_types(product_types)
+        if types is not None:
+            params['types'] = types
+        res = self._call_opus_api('files', 'json', params=params)
+        return res
+
+    @hide_paging('data')
+    def get_images_raw(self, query, startobs, limit, size=None):
+        """Return the results of raw calls to images.json.
+
+        This returns a list. Each list element is a dict where the key is
+        the image field and the value is the associated value.
+
+        Example:
+            If a size is not specified:
+
+            [{'full_alt_text': 'N1454725799_1_small.jpg',
+              'full_height': 256,
+              [...]
+              'full_url': 'https://pds-rings.seti.org/ ... N1454725799_1_small.jpg',
+              'full_width': 256}]
+
+            If a size is specified:
+
+            [{'alt_text': 'N1454725799_1_small.jpg',
+              'height': 256,
+              [...]
+              'url': 'https://pds-rings.seti.org/ ... N1454725799_1_small.jpg',
+              'width': 256}]
+        """
+        params = {} if query is None else query.get_api_params(opusapi=self)
+        params['startobs'] = startobs
+        params['limit'] = limit
+        image_url = 'images'
+        if size is not None:
+            size = size.lower()
+            assert size in (None, 'thumb', 'small', 'med', 'full')
+            image_url += '/'+size
+        res = self._call_opus_api(image_url, 'json', params=params)
+        return res
